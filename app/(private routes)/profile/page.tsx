da910635f8b70f045349e4c45e97728a3195fa20
Vadym <vadim.drinevskiy@gmail.com>
2025-07-14 00:51:54 +0300

    add meta-tag to profile page

--- app/(private routes)/profile/page.tsx
+++ app/(private routes)/profile/page.tsx
@@ -3,6 +3,37 @@ import css from "./ProfilePage.module.css";
 import Link from "next/link";
 import { getMeServer } from "@/lib/api/serverApi";
 
+export const generateMetadata = async (): Promise<Metadata> => {
+  const { username, avatar } = await getMeServer();
+
+  return {
+    title: username,
+    description: `profile of ${username}`,
+
+    openGraph: {
+      title: username,
+      description: `profile of ${username}`,
+      url: `https://09-auth-omega.vercel.app/profile`,
+      siteName: "NoteHub",
+      images: [
+        {
+          url: avatar || "/default-avatar.jpg",
+          width: 1200,
+          height: 630,
+          alt: "NoteHub App",
+        },
+      ],
+      type: "article",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: username,
+      description: `profile of ${username}`,
+      images: [avatar || "/default-avatar.jpg"],
+    },
+  };
+};
+
 const ProfilePage = async () => {
   const user = await getMeServer();
   console.log(user);
